b42de0b4614508716fde52b9cfaaa6a54f8758a5
Vadim <Moisei2224@gmail.com>
2022-11-17 20:31:17 +0300

    Step 11, the initial layout of the social network using the grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Technologies from "./Technologies";
+import {Header} from "./Header";
+import {Footer} from "./Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className={'app-wrapper'}>
+          <header className={'header'}>
+              <img src="https://i.pinimg.com/originals/e4/74/0d/e4740d91e3e734fafeb1acbe0f14f467.jpg" alt=""/>
+          </header>
+          <nav className={'nav'}>
+              <div>
+                  <a>Profile</a>
+              </div>
+              <div>
+                  <a>Messages</a>
+              </div>
+              <div>
+                  <a>News</a>
+              </div>
+              <div>
+                  <a>Music</a>
+              </div>
+              <div>
+                  <a>Settings</a>
+              </div>
+          </nav>
+          <div className={'content'}>
+              <img src="https://st.depositphotos.com/1766887/2306/i/600/depositphotos_23063254-stock-photo-beautiful-white-sandy-beaches-of.jpg" alt=""/>
+              <div>
+                  ava + desc
+              </div>
+              <div>
+                  my posts
+              </div>
+              <div>
+                  New posts
+              </div>
+              <div>
+                  <div>Post 1</div>
+                  <div>Post 2</div>
+              </div>
+          </div>
+
+      </div>
+  )
 }
+
+
 
 export default App;
