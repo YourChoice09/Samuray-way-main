2cde7925c950d5c89a9192051ed181df86a9de12
Vadim <Moisei2224@gmail.com>
2022-11-18 01:20:48 +0300

    Step 14, Formation of CSS-styles (modules CSS) for Header, Navbar and Profile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 export const Header = () => {
     return (
-            <header className={'header'}>
+            <header className={s.header}>
                 <img src="https://i.pinimg.com/originals/e4/74/0d/e4740d91e3e734fafeb1acbe0f14f467.jpg" alt=""/>
             </header>
         )
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 export const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={s.content}>
             <img src="https://st.depositphotos.com/1766887/2306/i/600/depositphotos_23063254-stock-photo-beautiful-white-sandy-beaches-of.jpg" alt=""/>
             <div>
                 ava + desc
@@ -10,12 +11,12 @@ export const Profile = () => {
             <div>
                 my posts
             </div>
-            <div>
+            <div className={s.posts}>
                 New posts
             </div>
-            <div>
-                <div>Post 1</div>
-                <div>Post 2</div>
+            <div className={s.posts}>
+                <div className={s.item}>Post 1</div>
+                <div className={s.item}>Post 2</div>
             </div>
         </div>
         )
